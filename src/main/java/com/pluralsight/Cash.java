44949bbf04d386b5687b16c8d324ae39d61b5aa0
jesicamatul <evelinmtlj@gmail.com>
2025-10-29 11:49:59 -0400

    created an instance of asset

--- src/main/java/com/pluralsight/Cash.java
+++ src/main/java/com/pluralsight/Cash.java
@@ -3,6 +3,17 @@ package com.pluralsight;
 public class Cash extends Asset {
 
     public Cash(String description, String dateAcquired, double originalCost) {
-        super(description, dateAcquired, originalCost);
+        super(description,  dateAcquired , originalCost);
+
+    }
+
+    public Cash(String dateAcquired, double originalCost) {
+        super("$" + originalCost + " in CASH",  dateAcquired , originalCost);
+
+    }
+
+    public double getValue(){
+        return getOriginalCost();
     }
 }
+
---
+++ src/main/java/com/pluralsight/House.java
@@ -0,0 +1,62 @@
+package com.pluralsight;
+
+public class House extends Asset {
+    private String address;
+    private int condition;
+    private int squareFoot;
+    private int lotSize;
+
+    public House(String description, String dateAcquired, double originalCost, String address, int condition, int squareFoot, int lotSize) {
+        super(description, dateAcquired, originalCost);
+        this.address = address;
+        this.condition = condition;
+        this.squareFoot = squareFoot;
+        this.lotSize = lotSize;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getCondition() {
+        return condition;
+    }
+
+    public void setCondition(int condition) {
+        this.condition = condition;
+    }
+
+    public int getSquareFoot() {
+        return squareFoot;
+    }
+
+    public void setSquareFoot(int squareFoot) {
+        this.squareFoot = squareFoot;
+    }
+
+    public int getLotSize() {
+        return lotSize;
+    }
+
+    public void setLotSize(int lotSize) {
+        this.lotSize = lotSize;
+    }
+    @Override
+    public double getValue() {
+        double squareFtPrice = switch (condition) {
+            case 1 -> 180; //excellent
+            case 2 -> 130; //good
+            case 3 -> 90; //fair
+            case 4 -> 80; //poor
+            default -> 0;
+        };
+
+        return (squareFtPrice * this.squareFoot) + (.25 * this.lotSize);
+
+    }
+
+}
