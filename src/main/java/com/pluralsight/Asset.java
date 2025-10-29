f2bd80f43ae57a2832ccb302d424b485c7477481
jesicamatul <evelinmtlj@gmail.com>
2025-10-29 10:21:37 -0400

    created abstract class and added constructor

---
+++ src/main/java/com/pluralsight/Asset.java
@@ -0,0 +1,45 @@
+package com.pluralsight;
+
+public  abstract class Asset {
+    private String description; //instance variables
+    private String dateAcquired;
+    private double originalCost;
+
+    //constructor
+    public Asset(String description, String dateAcquired, double originalCost) {
+        this.description = description;
+        this.dateAcquired = dateAcquired;
+        this.originalCost = originalCost;
+    }
+
+    //getters/setters
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDateAcquired() {
+        return dateAcquired;
+    }
+
+    public void setDateAcquired(String dateAcquired) {
+        this.dateAcquired = dateAcquired;
+    }
+
+    public double getOriginalCost() {
+        return originalCost;
+    }
+
+    public void setOriginalCost(double originalCost) {
+        this.originalCost = originalCost;
+    }
+
+    //method value
+    public abstract double getValue();
+
+}
